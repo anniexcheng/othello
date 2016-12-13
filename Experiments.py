2ed36c29a8eb4937328eb11646a4e5f2228ada3c
Ji Hun Kim <jk2227@cornell.edu>
2016-12-13 18:26:09 -0500

    changed all the 10000 to 1000

--- Experiments.py
+++ Experiments.py
@@ -37,11 +37,11 @@ def printExperimentResults(number):
 
 # Random (1) vs Random (1)
 def experiment1():
-    return runExperiment(10000, 1, 1)
+    return runExperiment(1000, 1, 1)
 
 # One Move Naive Eval Function (2) vs Random (1)
 def experiment2():
-    return runExperiment(10000, 2, 1)
+    return runExperiment(1000, 2, 1)
 
 # 3 Moves Ahead Min Max (3) vs Random (1)
 def experiment3():
